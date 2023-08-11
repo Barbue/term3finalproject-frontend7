0fca0544b6be8d29d7299236a7fba5cfde926bb0
Barbue <david.crochet@codeimmersives.com>
2023-08-11 15:03:54 -0500

    changed endpoint and url to /api

--- src/Hooks/authUtils.js
+++ src/Hooks/authUtils.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-const url = "http://localhost:5001/api";
+const url = "/api";
+
+//const url = "http://localhost:5001/api";
 
 const registerUser = async (userData) => {
 	const response = await axios.post(`${url}/users/register`, userData);
